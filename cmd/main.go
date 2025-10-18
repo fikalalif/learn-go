167ec61738c8819f1a5594b33a3eb95401202411
fikalalif <fikalgans26@gmail.com>
2025-10-18 20:07:58 +0700

    konsep pointer di golang

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"project1/internal/array"
+	"project1/internal/slice"
+	"project1/internal/maps"
+	"project1/internal/structData"
+	"project1/internal/pointer"
+)
+
+func main() {
+	array.Array()
+	slice.Slice()
+	slice.CaseSlice()
+	maps.Map()	
+	structData.NewStruct()
+	pointer.PointerData()
+}
